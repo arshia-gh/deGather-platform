19534910efd4014b1cca5f663e92b1ae40137c69
Arshia Gh <gholami.arshia@outlook.com>
2022-11-15 05:09:12 +0800

    setup vite, react and tailwind

---
+++ web-client-react/tailwind.config.cjs
@@ -0,0 +1,21 @@
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
