9599896758d27d3daa62ddfcd60c2f9431771e11
Carricklie <b1902338@helplive.edu.my>
2023-04-07 17:06:36 +0800

    Bug fix

--- packages/core/blockchain-module/historyBook.js
+++ packages/core/blockchain-module/historyBook.js
@@ -25,71 +25,73 @@ export class HistoryBook{
     }
     loadData(blockchain){
         blockchain.chain.forEach(block => {
-            block.transactions.forEach(transaction => {
-                //forHistoryBook
-                this.transactionList.push(transaction);
-                if(transaction.data instanceof Form){
-                    this.formList.push(transaction.data);
-                }
-                if(transaction.data instanceof Response){
-                    this.formList.forEach(form => {
-                        if(transaction.data.formId==form.id){
-                            form.responses.push(transaction.data);
-                        }
-                    });
-                }
-                //createNewSender or get existing sender
-                if(transaction.sender!=this.smartContractAddress){
-                    var userSender = new User(transaction.sender);
-                    var isNewUser = true;
-                    for(var i=0;i<this.userList.length;i++){
-                        if(this.userList[i].address == userSender){
-                            userSender = this.userList[i];
-                            isNewUser = false;
-                        }
-                    }
-                    userSender.transactionList.push(transaction);
-                    userSender.spend(transaction.creditAmount);
+            if(block.transactions!="Genesis"){
+                block.transactions.forEach(transaction => {
+                    //forHistoryBook
+                    this.transactionList.push(transaction);
                     if(transaction.data instanceof Form){
-                        userSender.formList.push(transaction.data);
+                        this.formList.push(transaction.data);
                     }
-                    //update or insert to historyBook
-                    if(isNewUser){
-                        this.userList.push(userSender);
-                    }else{
+                    if(transaction.data instanceof Response){
+                        this.formList.forEach(form => {
+                            if(transaction.data.formId==form.id){
+                                form.responses.push(transaction.data);
+                            }
+                        });
+                    }
+                    //createNewSender or get existing sender
+                    if(transaction.sender!=this.smartContractAddress){
+                        var userSender = new User(transaction.sender);
+                        var isNewUser = true;
                         for(var i=0;i<this.userList.length;i++){
-                            if(this.userList[i].address==userSender.address){
-                                this.userList[i] = userSender;
-                                break;
+                            if(this.userList[i].address == userSender){
+                                userSender = this.userList[i];
+                                isNewUser = false;
+                            }
+                        }
+                        userSender.transactionList.push(transaction);
+                        userSender.spend(transaction.creditAmount);
+                        if(transaction.data instanceof Form){
+                            userSender.formList.push(transaction.data);
+                        }
+                        //update or insert to historyBook
+                        if(isNewUser){
+                            this.userList.push(userSender);
+                        }else{
+                            for(var i=0;i<this.userList.length;i++){
+                                if(this.userList[i].address==userSender.address){
+                                    this.userList[i] = userSender;
+                                    break;
+                                }
                             }
                         }
                     }
-                }
-                //createNewReceiver or get existing receiver
-                if(transaction.receiver!=this.smartContractAddress){
-                    var userReceiver = new User(transaction.receiver);
-                    var isNewUser = true;
-                    for(var i=0;i<this.userList.length;i++){
-                        if(this.userList[i].address == userReceiver){
-                            userReceiver = this.userList[i];
-                            isNewUser = false;
-                        }
-                    }
-                    userReceiver.transactionList.push(transaction);
-                    userReceiver.gain(transaction.creditAmount);
-                    //update or insert to historyBook
-                    if(isNewUser){
-                        this.userList.push(userReceiver);
-                    }else{
+                    //createNewReceiver or get existing receiver
+                    if(transaction.receiver!=this.smartContractAddress){
+                        var userReceiver = new User(transaction.receiver);
+                        var isNewUser = true;
                         for(var i=0;i<this.userList.length;i++){
-                            if(this.userList[i].address==userReceiver.address){
-                                this.userList[i] = userReceiver;
-                                break;
+                            if(this.userList[i].address == userReceiver){
+                                userReceiver = this.userList[i];
+                                isNewUser = false;
+                            }
+                        }
+                        userReceiver.transactionList.push(transaction);
+                        userReceiver.gain(transaction.creditAmount);
+                        //update or insert to historyBook
+                        if(isNewUser){
+                            this.userList.push(userReceiver);
+                        }else{
+                            for(var i=0;i<this.userList.length;i++){
+                                if(this.userList[i].address==userReceiver.address){
+                                    this.userList[i] = userReceiver;
+                                    break;
+                                }
                             }
                         }
                     }
-                }
-            });
+                });
+            }
         });
     }
 }
